fd3aef52e23c6d8b69d2007a44665f012f5f56bd
deltrex <deltrexgg@gmail.com>
2023-04-16 19:08:18 +0530

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,168 @@
+import com.veinhorn.forismatic.api.Forismatic;
+import com.veinhorn.forismatic.api.Language;
+import com.veinhorn.forismatic.api.Quote;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    public static WebDriver driver = null;
+    public static String stock;
+    public static String advice;
+    public static String quoteText;
+
+    public static void main(String args[]) throws Exception {
+        for (int i = 1; i > 0; i++) {
+
+            for (int j = 0; j < 25; j++) {
+
+
+                String[] location = {"[\"AAPL\",\"NASDAQ\"],\"Apple Inc\",0,\"USD\",[",
+                        "[\"MSFT\",\"NASDAQ\"],\"Microsoft Corp\",0,\"USD\",[",
+                        "[\"KO\",\"NYSE\"],\"Coca-Cola Co\",0,\"USD\",[",
+                        "[\"TSLA\",\"NASDAQ\"],\"Tesla Inc\",0,\"USD\",[",
+                        "[\"NFLX\",\"NASDAQ\"],\"Netflix Inc\",0,\"USD\",[",
+                        "[\"NDAQ\",\"NASDAQ\"],\"Nasdaq Inc\",0,\"USD\",[",
+                        "[\"SONY\",\"NYSE\"],\"Sony Group Corp\",6,\"USD\",[",
+                        "[\"DIS\",\"NYSE\"],\"Walt Disney Co\",0,\"USD\",[",
+                        "[\"PEP\",\"NASDAQ\"],\"PepsiCo, Inc.\",0,\"USD\",[",
+                        "[\"AMZN\",\"NASDAQ\"],\"Amazon.com, Inc.\",0,\"USD\",[",
+                        "[\"MCD\",\"NYSE\"],\"McDonald's Corp\",0,\"USD\",[",
+                        "[\"SBUX\",\"NASDAQ\"],\"Starbucks Corp\",0,\"USD\",[",
+                        "[\"ADDDF\",\"OTCMKTS\"],\"adidas AG\",0,\"USD\",[",
+                        "[\"NKE\",\"NYSE\"],\"Nike Inc\",0,\"USD\",[",
+                        "[\"SPOT\",\"NYSE\"],\"Spotify Technology SA\",0,\"USD\",[",
+                        "[\"META\",\"NASDAQ\"],\"Meta Platforms Inc\",0,\"USD\",[",
+                        "[\"GOOGL\",\"NASDAQ\"],\"Alphabet Inc Class A\",0,\"USD\",[",
+                        "[\"HMC\",\"NYSE\"],\"Honda Motor Co Ltd\",6,\"USD\",[",
+                        "[\"NSANY\",\"OTCMKTS\"],\"Nissan Motor Co Ltd\",6,\"USD\",[",
+                        "[\"NVDA\",\"NASDAQ\"],\"NVIDIA Corporation\",0,\"USD\",[",
+                        "[\"BABA\",\"NYSE\"],\"Alibaba Group Holding Ltd - ADR\",6,\"USD\",[",
+                        "[\"MU\",\"NASDAQ\"],\"Micron Technology Inc\",0,\"USD\",[",
+                        "[\"BLK\",\"NYSE\"],\"BlackRock Inc\",0,\"USD\",[",
+                        "[\"V\",\"NYSE\"],\"Visa Inc\",0,\"USD\",[",
+                        "[\"GME\",\"NYSE\"],\"GameStop Corp.\",0,\"USD\",["};
+
+                String[] weburl =
+                        {"https://www.google.com/finance/quote/AAPL:NASDAQ?hl=en",
+                                "https://www.google.com/finance/quote/MSFT:NASDAQ?hl=en",
+                                "https://www.google.com/finance/quote/KO:NYSE?hl=e",
+                                "https://www.google.com/finance/quote/TSLA:NASDAQ?hl=en",
+                                "https://www.google.com/finance/quote/NFLX:NASDAQ?hl=en",
+                                "https://www.google.com/finance/quote/NDAQ:NASDAQ?hl=en",
+                                "https://www.google.com/finance/quote/SONY:NYSE?hl=en",
+                                "https://www.google.com/finance/quote/DIS:NYSE?hl=en",
+                                "https://www.google.com/finance/quote/PEP:NASDAQ?hl=en",
+                                "https://www.google.com/finance/quote/AMZN:NASDAQ?hl=en",
+                                "https://www.google.com/finance/quote/MCD:NYSE?hl=en",
+                                "https://www.google.com/finance/quote/SBUX:NASDAQ?hl=en",
+                                "https://www.google.com/finance/quote/ADDDF:OTCMKTS?hl=en",
+                                "https://www.google.com/finance/quote/NKE:NYSE?hl=en",
+                                "https://www.google.com/finance/quote/SPOT:NYSE?hl=en",
+                                "https://www.google.com/finance/quote/META:NASDAQ?hl=en&window=5Y",
+                                "https://www.google.com/finance/quote/GOOGL:NASDAQ?hl=en",
+                                "https://www.google.com/finance/quote/HMC:NYSE?hl=en",
+                                "https://www.google.com/finance/quote/NSANY:OTCMKTS?hl=en",
+                                "https://www.google.com/finance/quote/NVDA:NASDAQ?hl=en",
+                                "https://www.google.com/finance/quote/BABA:NYSE?hl=en",
+                                "https://www.google.com/finance/quote/MU:NASDAQ?hl=en",
+                                "https://www.google.com/finance/quote/BLK:NYSE?hl=en",
+                                "https://www.google.com/finance/quote/V:NYSE?hl=en",
+                                "https://www.google.com/finance/quote/GME:NYSE?hl=en"};
+
+                String[] copname = {"#Apple", "#Microsoft", "#Coco-Cola", "#Tesla", "#Netflix", "#Nasdaq",
+                        "#Sony", "#Disney", "#Pepsi Co", "#Amazon", "#McDonald's", "#Starbucks Corp", "#Adidas AG",
+                        "#Nike Inc", "#Spotify", "#Meta", "#Alphabet Inc", "#Honda Motor Co Ltd", "#Nissan Motor Co Ltd",
+                        "#NVIDIA Corporation", "#Alibaba Group Holding Ltd - ADR", "#Micron Technology Inc", "#BlackRock Inc",
+                        "#Visa Inc", "#GameStop Corp"};
+
+
+                URL url = new URL(weburl[j]);
+                URLConnection urlConn = url.openConnection();
+                InputStreamReader inStream = new InputStreamReader(urlConn.getInputStream());
+                BufferedReader buff = new BufferedReader(inStream);
+                String line = buff.readLine();
+                int positionlen = location[j].length();
+                while (line != null) {
+                    if (line.contains(location[j])) {
+                        int num;
+                        num = line.indexOf(location[j]);
+                        int len = line.length();
+                        String newline = line.substring(num + positionlen, len);
+                        int coma = newline.indexOf(",");
+                        stock = copname[j] + " share price - $" + newline.substring(0, coma);
+                        System.out.println(stock);
+
+                        selenium call = new selenium();
+                        call.start();
+
+                        System.out.println("--------------");
+
+
+                    }
+                    line = buff.readLine();
+                }
+               Thread.sleep(1200000);
+               System.setProperty("webdriver.edge.driver","C:\\Users\\HARI NANDAN\\IdeaProjects\\selenium\\msedgedriver.exe");
+                driver = new EdgeDriver();
+                driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
+                driver.navigate().to("https://phrasegenerator.com/finance");
+
+                Thread.sleep(8000);
+
+                advice = driver.findElement(By.cssSelector("body > div > div.right-area > div > div.content-area > div.phrase-panel > div > div.phrase-inner > div")).getText();
+                /*
+                Random rand1 = new Random();
+                int random1 = rand1.nextInt(1, 4);
+                String[] financehashtag = {"#finance","#market","#news","#business"};
+                text = text + "       "+financehashtag[random1];
+
+                 */
+
+                System.out.println(advice);
+
+                selenium1 call1 = new selenium1();
+                call1.start();
+
+                System.out.println("--------------");
+
+                driver.close();
+
+                Thread.sleep(1200000);
+
+                Quote quote = new Forismatic(Language.ENGLISH).getQuote();
+                Forismatic forismatic = new Forismatic();
+                forismatic.setLanguage(Language.ENGLISH);
+                quoteText = quote.getQuoteText();
+
+                Random rand = new Random();
+                int random = rand.nextInt(1, 4);
+                String[] motivhashtag = {"#motivation","#inspiration","#success","#life"};
+                quoteText = quoteText  + "        "+motivhashtag[random];
+                System.out.println(quoteText);
+
+                selenium2 call2 = new selenium2();
+                call2.start();
+
+
+                System.out.println("-------------");
+                Thread.sleep(1200000);
+
+            }
+
+                }
+            }
+        }
+
+
+
+
+
---
+++ src/main/java/selenium.java
@@ -0,0 +1,115 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.util.concurrent.TimeUnit;
+
+public class selenium extends Thread{
+    public static WebDriver driver = null;
+    @Override
+    public void run() {
+
+        String totweet = Main.stock;
+
+
+
+        System.setProperty("webdriver.edge.driver","C:\\Users\\HARI NANDAN\\IdeaProjects\\selenium\\msedgedriver.exe");
+        driver = new EdgeDriver();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
+        driver.navigate().to("https://twitter.com/i/flow/login");
+
+        WebElement username = driver.findElement(By.cssSelector("#layers > div > div > div > div > div > div > div.css-1dbjc4n.r-1awozwy.r-18u37iz." +
+                "r-1pi2tsx.r-1777fci.r-1xcajam.r-ipm5af.r-g6jmlv > div.css-1dbjc4n.r-1867qdf.r-1wbh5a2.r-kwpbio.r-rsyp9y.r-1pjcn9w.r-1279nm1.r-" +
+                "htvplk.r-1udh08x > div > div > div.css-1dbjc4n.r-14lw9ot.r-6koalj.r-16y2uox.r-1wbh5a2 > div.css-1dbjc4n.r-16y2uox.r-1wbh5a2.r-1jgb5lz.r" +
+                "-1ye8kvj.r-13qz1uu > div > div > div > div.css-1dbjc4n.r-mk0yit.r-1f1sjgu.r-13qz1uu > label > div > div.css-1dbjc4n.r-18u37iz.r-16y2uox.r-" +
+                "1wbh5a2.r-1wzrnnt.r-1udh08x.r-xd6kpl.r-1pn2ns4.r-ttdzmv > div > input"));
+        username.sendKeys("deltrexgg");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            Robot robot = new Robot();
+            robot.keyPress(KeyEvent.VK_ENTER);
+            robot.keyRelease(KeyEvent.VK_ENTER);
+        } catch (AWTException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        WebElement password = driver.findElement(By.cssSelector("#layers > div > div > div > div > div > div > div.css-1dbjc4n.r-1awozw" +
+                "y.r-18u37iz.r-1pi2tsx.r-1777fci.r-1xcajam.r-ipm5af.r-g6jmlv > div.css-1dbjc4n.r-1867qdf.r-1wbh5a2.r-kwpbio.r-rsyp" +
+                "9y.r-1pjcn9w.r-1279nm1.r-htvplk.r-1udh08x > div > div > div.css-1dbjc4n.r-14lw9ot.r-6koalj.r-16y2uox.r-1wbh5a2 > div.css-1dbj" +
+                "c4n.r-16y2uox.r-1wbh5a2.r-1jgb5lz.r-1ye8kvj.r-13qz1uu > div.css-1dbjc4n.r-16y2uox.r-1wbh5a2.r-1dqxon3 > div > div > div.css-1dbjc4n.r-mk0yit" +
+                ".r-13qz1uu > div > label > div > div.css-1dbjc4n.r-18u37iz.r-16y2uox.r-1wbh5a2.r-1wzrnnt.r-1udh08x.r-xd6kpl.r-1pn2ns4.r-ttdzmv > div.css-901oao.r-1awozwy." +
+                "r-6koalj.r-37j5jr.r-1inkyih.r-16dba41.r-135wba7.r-bcqeeo.r-13qz1uu.r-qvutc0 > input"));
+        password.sendKeys("Nainaremesh*1");
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            Robot robot = new Robot();
+            robot.keyPress(KeyEvent.VK_ENTER);
+            robot.keyRelease(KeyEvent.VK_ENTER);
+        } catch (AWTException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        WebElement tweetbox = driver.findElement(By.cssSelector("#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div " +
+                "> div.css-1dbjc4n.r-14lw9ot.r-jxzhtn.r-1ljd8xs.r-13l2t4g.r-1phboty.r-16y2uox.r-1jgb5lz.r-11wrixw.r-61z16t.r-1ye8kvj.r-13qz1uu.r-184en5c > div > " +
+                "div.css-1dbjc4n.r-14lw9ot.r-184en5c > div > div.css-1dbjc4n.r-14lw9ot.r-oyd9sg > div:nth-child(1) > div > div > div > div.css-1dbjc4n.r-1iusvr4.r-" +
+                "16y2uox.r-1777fci.r-1h8ys4a.r-1bylmt5.r-13tjlyg.r-7qyjyx.r-1ftll1t > div.css-1dbjc4n.r-184en5c > div > div > div > div > div > div.css-1dbjc4n.r-" +
+                "16y2uox.r-bnwqim.r-13qz1uu.r-1g40b8q > div > div > div > div > label > div.css-1dbjc4n.r-16y2uox.r-1wbh5a2 > div > div > div > div > div > div." +
+                "DraftEditor-editorContainer > div"));
+        tweetbox.sendKeys(totweet);
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        WebElement tweet = (WebElement) driver.findElement(By.cssSelector("#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 " +
+                "> main > div > div > div > div.css-1dbjc4n.r-14lw9ot.r-jxzhtn.r-1ljd8xs.r-13l2t4g.r-1phboty.r-16y2uox.r-1jgb5lz.r-11wrixw.r-61z16t.r-1y" +
+                "e8kvj.r-13qz1uu.r-184en5c > div > div.css-1dbjc4n.r-14lw9ot.r-184en5c > div > div.css-1dbjc4n.r-14lw9ot.r-oyd9sg > div:nth-child(1) > div >" +
+                " div > div > div.css-1dbjc4n.r-1iusvr4.r-16y2uox.r-1777fci.r-1h8ys4a.r-1bylmt5.r-13tjlyg.r-7qyjyx.r-1ftll1t > div:nth-child(3) > div > div >" +
+                " div:nth-child(2) > div.css-18t94o4.css-1dbjc4n.r-l5o3uw.r-42olwf.r-sdzlij.r-1phboty.r-rs99b7.r-19u6a5r.r-2yi16.r-1qi8awa.r-1ny4l3l.r-ymttw5.r-o7ynqc" +
+                ".r-6416eg.r-lrvibr > div > span > span"));
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        tweet.click();
+        driver.close();
+
+
+
+
+
+    }
+
+
+
+}
